7bd098189edc232fbceaa8c77e4c266df4fac499
Daniel J. Szelogowski <dszelogowski@gmail.com>
2023-12-30 19:03:38 -0600

    Added date/time configuration

--- src/IceCream.jl
+++ src/IceCream.jl
@@ -11,8 +11,8 @@ export @ic, ic_configure, ic_configure_color, IC_Color
 # Define default configurations using Refs for mutable global state
 const DEFAULT_PREFIX = "ic| "
 const ICE_CREAM_ENABLED = Ref(true)
-const CONTEXT_INFO = Ref((include_context=true, include_filename=false, 
-                         include_classname=true, include_absolute_path=false, 
+const CONTEXT_INFO = Ref((include_context=true, include_datetime=true, include_filename=false, 
+                         include_classname=true, include_absolute_path=false,
                          include_methodname=true, include_line_number=true))
 const ERROR_CONTEXT_DISABLED = "includeContext needs to be enabled to set this config"
 const DEFAULT_COLOR = :cyan
@@ -22,15 +22,15 @@ color_output = Ref(true) # Control color output
 
 # Configuration function to set various options
 function ic_configure(; new_prefix=DEFAULT_PREFIX, enabled=true, include_context=true, 
-                      include_filename=false, include_classname=true, 
+                      include_datetime=true, include_filename=false, include_classname=true, 
                       include_absolute_path=false, include_methodname=true, 
                       include_line_number=true, color=true)
     ICE_CREAM_ENABLED[] = enabled
-    CONTEXT_INFO[] = (include_context=include_context, include_filename=include_filename, 
+    CONTEXT_INFO[] = (include_context=include_context, include_datetime=include_datetime, include_filename=include_filename, 
                       include_classname=include_classname, include_absolute_path=include_absolute_path,
                       include_methodname=include_methodname, include_line_number=include_line_number)
     color_output[] = color
-    if !include_context && (include_filename || include_classname || include_absolute_path || 
+    if !include_context && (include_datetime || include_filename || include_classname || include_absolute_path || 
                             include_methodname || include_line_number)
     error(ERROR_CONTEXT_DISABLED)
 end
@@ -73,7 +73,7 @@ macro ic(exprs...)
                 field_value = getfield(context_info_tuple, field_name)
                 s_field_name = string(field_name)
                 if field_value && do_context
-                    if s_field_name == "include_context"
+                    if s_field_name == "include_datetime"
                         output_str *= "[$timestamp] "
                     elseif s_field_name == "include_absolute_path"
                         output_str *= "$(abspath(@__FILE__))/"
